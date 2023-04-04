a565eb3bd345394fdd092b2f7a7a548de1d12f41
Stidz-PC <marszp8888@gmail.com>
2023-04-04 19:18:06 +0200

    Addde application properties and SpringBootTests

---
+++ src/test/java/com/msz/SpringBootAlgorithmApi/SpringBootAlgorithmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msz.SpringBootAlgorithmApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAlgorithmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
